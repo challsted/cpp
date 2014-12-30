56fe1fa8ee9aac2c75ac1b398a4cfa78a578a451
hackinginformation <chase.hallsted2012@gmail.com>
2014-12-30 10:51:20 -0600

    Completed problem 15

---
+++ ch2/15.C
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/*
+ * This program is for Section 2-13 Question 15 found on page 69 
+ * of the book "Computer Science A Structured Programming Approach Useing C++"
+ */
+
+using namespace std;
+
+int main (void)
+{
+    int num1 = 1;
+    int num100 = 100;
+    int num1k = 1000;
+    int num10k = 10000;
+    char space = ' ';
+
+    cout << num1 << space
+         << num100 << space
+         << num1k << space
+         << num10k << space
+         << endl;
+    return 0;
+}
