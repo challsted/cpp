c6c10fb4ec7ef5163b99eec3ce0cd6ab5723465e
hackinginformation <chase.hallsted2012@gmail.com>
2014-12-30 10:28:59 -0600

    Fixed comment at top of 13

---
+++ ch2/13.C
@@ -0,0 +1,17 @@
+#include <iostream>
+
+/*
+ * This program is for Section 2-13 Question 14 found on page 69
+ * of the book "Computer Science A Structured Programming Approach Useing C++"
+ */
+
+using namespace std;
+
+int main (void)
+{
+    cout << "*" << endl;
+    cout << "**" << endl;
+    cout << "***" << endl;
+    cout << "****" << endl;
+    return 0;
+}
